c3bebde1709e9c58e8255f1b2a68061303271bd9
azambrano-ctrl <azambrano@troncal.net>
2026-01-24 17:41:15 -0500

    fix(build): add local multer types

---
+++ backend/src/types/multer.d.ts
@@ -0,0 +1,27 @@
+declare module 'multer' {
+    const multer: any;
+    export = multer;
+}
+
+declare namespace Express {
+    namespace Multer {
+        interface File {
+            fieldname: string;
+            originalname: string;
+            encoding: string;
+            mimetype: string;
+            size: number;
+            destination: string;
+            filename: string;
+            path: string;
+            buffer: Buffer;
+        }
+    }
+
+    interface Request {
+        files?: {
+            [fieldname: string]: Multer.File[];
+        } | Multer.File[];
+        file?: Multer.File;
+    }
+}
