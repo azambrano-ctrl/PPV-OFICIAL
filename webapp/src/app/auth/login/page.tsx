7e19f929e1030cccb89d166e503da611fdfe5036
azambrano-ctrl <azambrano.ctrl@gmail.com>
2026-02-11 21:00:47 -0500

    feat: aumentar tamaño de logos sociales de w-5 a w-6

--- webapp/src/app/auth/login/page.tsx
+++ webapp/src/app/auth/login/page.tsx
@@ -199,7 +199,7 @@ export default function LoginPage() {
                             onClick={() => handleSocialLogin('google')}
                             className="flex items-center justify-center py-3 bg-white/5 border border-white/5 hover:bg-white/10 rounded-xl transition-all text-white text-[9px] font-black uppercase tracking-widest"
                         >
-                            <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
+                            <svg className="w-6 h-6 mr-2" viewBox="0 0 24 24">
                                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
                                 <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
                                 <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
@@ -212,7 +212,7 @@ export default function LoginPage() {
                             onClick={() => handleSocialLogin('facebook')}
                             className="flex items-center justify-center py-3 bg-white/5 border border-white/5 hover:bg-white/10 rounded-xl transition-all text-white text-[9px] font-black uppercase tracking-widest"
                         >
-                            <svg className="w-5 h-5 mr-2" fill="#1877F2" viewBox="0 0 24 24">
+                            <svg className="w-6 h-6 mr-2" fill="#1877F2" viewBox="0 0 24 24">
                                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2" />
                                 <path d="M16.671 15.543l.532-3.47h-3.328v-2.25c0-.949.465-1.874 1.956-1.874h1.514V4.996s-1.374-.235-2.686-.235c-2.741 0-4.533 1.662-4.533 4.669v2.643H7.078v3.47h3.047v8.385a12.09 12.09 0 003.75 0v-8.385h2.796z" fill="white" />
                             </svg>
