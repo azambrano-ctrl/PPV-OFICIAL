32b6c613eaf790f21f6f76a8751f7d1eaef5194d
azambrano-ctrl <azambrano.ctrl@gmail.com>
2026-02-18 00:05:24 -0500

    fix(mobile): use native expo-preset import.meta polyfill and reset metro config

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo'],
+        presets: [
+            ['babel-preset-expo', {
+                unstable_transformImportMeta: true,
+            }],
+        ],
         plugins: [
-            // Required for expo-router
-            'expo-router/babel',
+            'react-native-reanimated/plugin',
         ],
     };
 };
